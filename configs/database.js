58595835fb276b89ee9d7788e4b092814f231961
unknown <mesbaul.cse26@gmail.com>
2023-05-25 17:57:15 +0600

    update docker file

--- configs/database.js
+++ configs/database.js
@@ -3,6 +3,7 @@ module.exports = {
 	user: process.env.DB_USER || 'root',
 	password: process.env.DB_PASSWORD || '',
 	database: process.env.DB_DATABASE || 'test',
+	port: process.env.DB_PORT || 3306,
 	dialect: "mysql",
 	pool: {
 		max: 5,
